cd8ced79946ab307d89dbf2647af59853f2bcd1b
thethepai <u202112003@hust.edu.cn>
2024-08-30 05:23:11 +0800

    edit profile

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -7,8 +7,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://www.jackpai.life/' },
-    { title: '希望你在这里收获快乐和灵感', url: 'https://www.jackpai.life/' }
+    // { title: '欢迎来到我的博客', url: 'https://www.jackpai.life/' },
+    // { title: '希望你在这里收获快乐和灵感', url: 'https://www.jackpai.life/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,14 +20,14 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'JACKPAI.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'GraphRAG探索',
-  HEO_HERO_TITLE_LINK: 'https://github.com/microsoft/graphrag',
+  HEO_HERO_TITLE_4: '希望你在这里获得快乐和创意',
+  HEO_HERO_TITLE_5: '欢迎你的到来',
+  HEO_HERO_TITLE_LINK: 'https://www.jackpai.life/',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '实验记录', url: '/tag/实验' },
+  HEO_HERO_CATEGORY_2: { title: '推荐文章', url: '/tag/推荐' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/开发' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
